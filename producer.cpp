50af2223497491fdaf05687a95ced6624787ab8a
jto05 <julian.to2023@gmail.com>
2025-11-16 12:53:53 -0800

    Implemented basic general Producer function; includes static variables for shared references to data between Producers.

--- producer.cpp
+++ producer.cpp
@@ -1,1 +1,33 @@
 #include "producer.h"
+
+#include <time.h>
+
+Producer::Producer( Monitor *monitor,
+    unsigned int sleepTime  ) : Robot(sleepTime) {
+  this->sleepTime = sleepTime; // sleepTime should be in milliseconds;
+
+}
+
+void Producer::start() {
+
+  struct timespec SleepTime;
+  SleepTime.tv_sec = sleepTime / 1000000;
+  SleepTime.tv_nsec = (sleepTime % 1000000 ) / 10000;
+
+
+  while ( requestsAdded <= maxRequests ) {
+
+    // sleep to simulate time it takes to complete request
+    nanosleep( &SleepTime, NULL );
+    monitor->insert( false );
+
+  }
+
+};
+
+
+
+void Producer::init_shared_data(int n ) {
+  this->maxRequests = n;
+  this->requestsAdded = 0;
+};
--- producer.h
+++ producer.h
@@ -1,5 +1,24 @@
 #ifndef PRODUCER_H
 #define PRODUCER_H
 
+#include <pthread.h>
+#include <queue>
+
+#include "monitor.h"
+#include "robot.h"
+
+using namespace std;
+
+class Producer : public Robot{
+private:
+  int currRequests;
+  static int maxRequests;
+  static int requestsAdded;
+
+public:
+  Producer( Monitor*, unsigned int );
+  void start();
+  static init_shared_data( int );
+};
 
 #endif
