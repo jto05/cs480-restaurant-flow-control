a926ace258f19fdac01cd268f590c492b863001b
jto05 <julian.to2023@gmail.com>
2025-11-16 18:07:00 -0800

    Implemented general Consumer class

--- consumer.cpp
+++ consumer.cpp
@@ -1,1 +1,31 @@
 #include "consumer.h"
+
+#include <iostream>
+
+Consumer::Consumer( Monitor *m, unsigned int sleepTime ) : Robot(sleepTime) {
+  this->monitor = m;
+}
+
+void Consumer::start() {
+
+  struct timespec SleepTime;
+  SleepTime.tv_sec = sleepTime / 1000;
+  SleepTime.tv_nsec = (sleepTime % 1000 ) / 1000000;
+
+  while ( monitor->addedRequests != monitor->maxRequests 
+      || monitor->requestsInQueue != 0 ) {
+
+    monitor->remove();
+  //  std::cout << "c sleping" << std::endl;
+    nanosleep( &SleepTime, NULL );
+
+  }
+  
+  std::cout << "c finish" << std::endl;
+
+}
+
+void *consumer_start( void* ptr ) {
+  Consumer *c = ( Consumer* ) ptr;
+  c->start();
+}
--- consumer.h
+++ consumer.h
@@ -1,4 +1,18 @@
 #ifndef CONSUMER_H
 #define CONSUMER_H
 
+#include "monitor.h"
+#include "robot.h"
+
+class Consumer : public Robot {
+private:
+  Monitor *monitor;
+
+public:
+  Consumer( Monitor*, unsigned int );
+  void start();
+};
+
+void *consumer_start( void* );
+
 #endif
