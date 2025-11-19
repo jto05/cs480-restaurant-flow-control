29564b726a16964c9f931e808d86f37e0f28c50b
jto05 <julian.to2023@gmail.com>
2025-11-18 21:03:48 -0800

    Added condition to signaling condition variables to ensure efficient signaling

--- monitor.cpp
+++ monitor.cpp
@@ -1,9 +1,25 @@
+/*
+ * Author: Julian To and Kaylee Muckerman
+ * REDID: 130405272 and 130526510
+ *
+  * Source code for Monitor class
+ */
+
 #include "monitor.h"
 
 #include <iostream>
 #include "seating.h"
 #include "log.h"
 
+/*
+ * Monitor(int, int, int)
+ *
+ * @brief A constructor for a Consumer object
+ *
+ * @param capacity of queue (20 should always be passed in)
+ * @param the maximum amount of requests to process 
+ * @param the maximum amount of VIPs to seat ( should always be 6 )
+ */
 Monitor::Monitor(int queueSize, int maxRequests, int vipLimit) {
   this->capacity = queueSize;
   this->vipLimit = vipLimit;
@@ -11,7 +27,7 @@ Monitor::Monitor(int queueSize, int maxRequests, int vipLimit) {
   this->totalAddedRequests = 0;
   this->requestsInQueue = 0;
 
-  //initialize consumed
+  //initialize consumed logging values
   consumed = new unsigned int*[ConsumerTypeN];
   for ( int i = 0; i < RequestTypeN; i ++ ) {
     consumed[i] = new unsigned int[RequestTypeN]();
@@ -24,17 +40,16 @@ Monitor::Monitor(int queueSize, int maxRequests, int vipLimit) {
 
 };
 
-void Monitor::processMaxRequestLimit() {
-
-};
-
 bool Monitor::insert( RequestType request ) {
+
+  bool wasEmpty;
 
       // * BEGIN CRITICAL REGION *
   pthread_mutex_lock( &lock );
 
-  // unlock mutex and then return true that we completed 
+  // unlock mutex and then return true that weve completed producing
   if ( totalAddedRequests >= maxRequests ) {
+    // ensure consumers will not be waiting  
     pthread_cond_signal( &unconsumed );
     pthread_mutex_unlock( &lock );
     return true;
@@ -43,10 +58,13 @@ bool Monitor::insert( RequestType request ) {
 
   //while queue is full
   while (requestsInQueue >= capacity) {
+
+    // wait thread until there are available slots in queue
     pthread_cond_wait( &availableSlots, &lock );
 
     // while waiting, we might hit maxRequests so we still return true here
     if ( totalAddedRequests >= maxRequests ) {
+      // ensure consumers will not be waiting  
       pthread_cond_signal( &unconsumed );
       pthread_mutex_unlock( &lock );
       return true;
@@ -54,8 +72,12 @@ bool Monitor::insert( RequestType request ) {
   }
 
   if ( request == VIPRoom ) {
+
     while ( inRequestQueue[request] >= vipLimit ) {
+
+      // wait thread until there are available VIP slots in queue
       pthread_cond_wait( &availableVIP, &lock );
+
       // while waiting, we might hit maxRequests so we still return true here
       if ( totalAddedRequests >= maxRequests ) {
         pthread_cond_signal( &unconsumed );
@@ -65,51 +87,92 @@ bool Monitor::insert( RequestType request ) {
     }
   } 
   
+  wasEmpty = queue.empty();
+
+
+  // push request to back of queue
   queue.push( request );
 
+
+
+  // logging values
   produced[request]++;
   inRequestQueue[request]++;
-
   requestsInQueue++;
   totalAddedRequests++;
 
+  // logging function
   output_request_added(request, produced, inRequestQueue);
 
-  pthread_cond_signal( &unconsumed );
+  // only signal to consumers if our queue was initially empty
+  if ( wasEmpty ) {
+    pthread_cond_signal( &unconsumed );
+  }
+
 
   pthread_mutex_unlock( &lock );
       // * END CRITICAL REGION *
+  
+  // unless we have reached max requests, return false that we are NOT done
   return false;
 }
 
+
+/*
+ * remove(ConsumerType)
+ *
+ * @brief Removes from the queue in FIFO order, signaling to
+ *        the producer that there is space in the queue 
+ *
+ * @param takes a ConsumerType for logging purposes
+ *
+ */
 void Monitor::remove( ConsumerType type ) {
+
+  bool atCapacity;
 
       // * BEGIN CRITICAL REGION *
   pthread_mutex_lock( &lock );
 
   // while queue is empty
   while (requestsInQueue <= 0) {
-    if (totalAddedRequests >= maxRequests) { // nothing else will arrive
+
+    // a hard exit out of loop if maxRequests is far
+    if (totalAddedRequests >= maxRequests) { 
      pthread_mutex_unlock(&lock);
-     return;  // caller should treat "no item" as time to exit
+     return;  
     }
+
+    // wait consumer until signaled by producer that there are 
+    //    requests to consume
     pthread_cond_wait( &unconsumed, &lock );
   }
 
+  atCapacity = ( requestsInQueue >= capacity );
+
+  // get request from front of queue 
   RequestType r = queue.front();
   queue.pop();
 
+  // logging values
   inRequestQueue[r]--;
   consumed[type][r]++;
   requestsInQueue--;
 
+  // logging function
   output_request_removed(type, r, consumed[type], inRequestQueue);
 
-  if ( r == VIPRoom ) 
-    pthread_cond_signal( &availableVIP );
-  
+  if ( atCapacity ) {
+    // signal to the producer that there are slots available
+    //    if it WAS at capacity
+    pthread_cond_signal( &availableSlots );
 
-  pthread_cond_signal( &availableSlots );
+    if ( r == VIPRoom )  {
+      // signal to the producer that there are VIP slots available
+      //    if it WAS at capacity
+      pthread_cond_signal( &availableVIP );
+    }
+  }
 
   pthread_mutex_unlock( &lock );
       // * END CRITICAL REGION *
--- monitor.h
+++ monitor.h
@@ -1,3 +1,10 @@
+/*
+ * Author: Julian To and Kaylee Muckerman
+ * REDID: 130405272 and 130526510
+ *
+  * Header for Monitor class
+ */
+
 #ifndef MONITOR_H
 #define MONITOR_H
 
@@ -6,9 +13,22 @@
 #include <pthread.h>
 #include <semaphore.h>  
 
+/*
+ * Monitor class encapsulates critical region of 
+ * consumer/producer interactions, facilitating
+ * the modification of the request queue
+ *
+ * Also acts as a container for shared variables
+ * used in both the Producer and Consumer class
+ * methods
+ *
+ */
 class Monitor { 
 private:
+  // request queue
   std::queue<RequestType> queue;
+
+  // condition variables and mutex
   pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
   pthread_cond_t unconsumed;
   pthread_cond_t availableSlots;
@@ -16,22 +36,54 @@ private:
 
 
 public:
-  sem_t consumersCompleted;
-  int requestsInQueue;
-  int vipLimit;
-  int totalAddedRequests;
-  int maxRequests;
-  int capacity;
+  // shared semaphore for Consumer completion
+  sem_t consumersCompleted; 
 
-  //logging
+  int capacity;
+  int vipLimit;
+  int maxRequests;
+  int totalAddedRequests;
+  int requestsInQueue;
+
+  //logging variables
   unsigned int produced[RequestTypeN];
   unsigned int **consumed;
   unsigned int inRequestQueue[RequestTypeN];
 
+  /*
+   * Monitor(int, int, int)
+   *
+   * @brief A constructor for a Consumer object
+   *
+   * @param capacity of queue (20 should always be passed in)
+   * @param the maximum amount of requests to process 
+   * @param the maximum amount of VIPs to seat ( should always be 6 )
+   */
   Monitor(int queueSize, int requestLimit, int vipLimit);
+
+  /*
+   * insert(RequestType)
+   *
+   * @brief simulates insertion of request into queue in a
+   *        mutually exclusive way, signaling to the consumers
+   *        that there are requests to be consumed
+   *
+   * @param takes a RequestType to add into queue
+   *
+   * @return returns if max amount of requests have been reached
+   */
   bool insert( RequestType );
+
+  /*
+   * remove(ConsumerType)
+   *
+   * @brief Removes from the queue in FIFO order, signaling to
+   *        the producer that there is space in the queue 
+   *
+   * @param takes a ConsumerType for logging purposes
+   *
+   */
   void remove( ConsumerType );
-  void processMaxRequestLimit();
 
 };
 
